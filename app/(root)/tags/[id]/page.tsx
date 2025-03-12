f396ddddf0718bb4dbb49d228e83e2daceaebbac
Tien Tran <tientran6110@gmail.com>
2025-03-12 17:50:01 +0100

    Show Top Questions and Tags

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,15 +1,10 @@
-import { Button } from "@mdxeditor/editor";
-import { Link } from "lucide-react";
-import React from "react";
-
 import QuestionCard from "@/components/cards/QuestionCard";
 import DataRenderer from "@/components/DataRenderer";
-import HomeFilter from "@/components/filters/HomeFilter";
+import Pagination from "@/components/Pagination";
 import LocalSearch from "@/components/search/LocalSearch";
 import ROUTES from "@/constants/routes";
 import { EMPTY_QUESTION } from "@/constants/states";
 import { getTagQuestions } from "@/lib/actions/tag.action";
-import Pagination from "@/components/Pagination";
 
 const Page = async ({ params, searchParams }: RouteParams) => {
   const { id } = await params;
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -15,6 +15,7 @@ import {
   IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
+import dbConnect from "../mongoose";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -319,6 +320,23 @@ export async function incrementViews(
     return {
       success: true,
       data: { views: question.views },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<ActionResponse<Question[]>> {
+  try {
+    await dbConnect();
+
+    const questions = await Question.find()
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(questions)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
