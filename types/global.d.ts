9017f607fcc0e2e0bd5ddc5b0864f785ab16f4a8
Tien Tran <tientran6110@gmail.com>
2025-04-21 13:41:35 +0200

    Implement Jobs Page

--- types/global.d.ts
+++ types/global.d.ts
@@ -97,3 +97,23 @@ interface Badges {
   SILVER: number;
   BRONZE: number;
 }
+
+interface Job {
+  id?: string;
+  employer_name?: string;
+  employer_logo?: string | undefined;
+  employer_website?: string;
+  job_employment_type?: string;
+  job_title?: string;
+  job_description?: string;
+  job_apply_link?: string;
+  job_city?: string;
+  job_state?: string;
+  job_country?: string;
+}
+
+interface Country {
+  name: {
+    common: string;
+  };
+}
