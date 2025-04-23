ebd9cce4cfa82d36355cfc1f6f0a0badcf8bf666
Tien Tran <tientran6110@gmail.com>
2025-04-23 13:54:00 +0200

    Implement Edit Profile Feature

--- lib/validations.ts
+++ lib/validations.ts
@@ -231,3 +231,37 @@ export const CreateInteractionSchema = z.object({
   actionId: z.string().min(1),
   authorId: z.string().min(1),
 });
+
+export const ProfileSchema = z.object({
+  name: z
+    .string()
+    .min(3, {
+      message: "Name must be at least 3 characters.",
+    })
+    .max(130, { message: "Name musn't be longer then 130 characters." }),
+  username: z
+    .string()
+    .min(3, { message: "username musn't be longer then 100 characters." }),
+  portfolio: z.string().url({ message: "Please provide valid URL" }),
+  location: z.string().min(3, { message: "Please provide proper location" }),
+  bio: z.string().min(3, {
+    message: "Bio must be at least 3 characters.",
+  }),
+});
+
+export const UpdateUserSchema = z.object({
+  name: z
+    .string()
+    .min(3, {
+      message: "Name must be at least 3 characters.",
+    })
+    .max(130, { message: "Name musn't be longer then 130 characters." }),
+  username: z
+    .string()
+    .min(3, { message: "username musn't be longer then 100 characters." }),
+  portfolio: z.string().url({ message: "Please provide valid URL" }),
+  location: z.string().min(3, { message: "Please provide proper location" }),
+  bio: z.string().min(3, {
+    message: "Bio must be at least 3 characters.",
+  }),
+});
